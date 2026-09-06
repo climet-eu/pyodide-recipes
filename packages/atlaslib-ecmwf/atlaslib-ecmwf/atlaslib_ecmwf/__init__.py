03021429971cadbfed321b0c72873237cae24d77
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2026-09-06 05:00:05 +0000

    add atlaslib-ecmwf eccodeslib mirlib mir-python packages

---
+++ packages/atlaslib-ecmwf/atlaslib-ecmwf/atlaslib_ecmwf/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.46.0.21'
+__commit_hash__ = 'e0af59ef4d895ffc647a9db01b48c0a3e13d16f6'
+findlibs_dependencies = ["eckitlib"]
---
+++ packages/mirlib/mirlib/mirlib/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '1.29.0.21'
+__commit_hash__ = '3e82d8b526a2f7333b210f2031863f10b6378cb1'
+findlibs_dependencies = ["eccodeslib", "eckitlib", "atlaslib_ecmwf"]
